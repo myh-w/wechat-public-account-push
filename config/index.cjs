af880b1aebc7efe07c6e26f2511c7e09981e6a60
myh-w <116645855+myh-w@users.noreply.github.com>
2025-03-17 22:21:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o76wX6mmfuFIjDA3ONY-AU2wtjgE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4nm5w17tDi3-3Y8LxP68Rwu1DmQv7XNJDuW-BwL-tb8',
+      useTemplateId: 'HT1oC7-c_X4IP6dnf9EGNaqQ_5NGcli0ST2j3epQFiM',
       //gwMNfXw2ata9464KLNfK7p_33ff5Mf63ofG0X-d3aD0
       //j4jgxtSwJ27lpnn-hLphFtcIKJftRHlORONRZbUxh4E
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
